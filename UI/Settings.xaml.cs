5295acd8cecd58f91d1923a238a33f37c71e78b0
Kurumi4019 <Kurumi4019@N1565CAW-E3>
2022-07-23 23:10:34 +0900

    DynamicAero2を実装したかった(変更しようとすると落ちます)

--- UI/Settings.xaml.cs
+++ UI/Settings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DynamicAero2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +10,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,11 +20,26 @@ namespace mp4Utl.NewFolder
 /// <summary>
 /// Settings.xaml の相互作用ロジック
 /// </summary>
-{ public partial class Settings : Window
+{
+    public partial class Settings : Window
     {
         public Settings()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Theme(object sender, RoutedEventArgs e)
+        {
+            var theme = Application.Current.Resources.MergedDictionaries[0] as DynamicAero2.Theme;
+            if (theme is null) return;
+
+            theme.Color = theme.Color switch
+            {
+                ThemeColor.NormalColor => ThemeColor.Black,
+                ThemeColor.Black => ThemeColor.Dark,
+                ThemeColor.Dark => ThemeColor.Light,
+                _ => ThemeColor.NormalColor
+            };
         }
     }
 }
