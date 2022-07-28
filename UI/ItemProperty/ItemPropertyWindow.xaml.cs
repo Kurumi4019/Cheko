7fa612f09a8c9a934f56e74e04a56ee7f2863320
Nodoka4318 <78198198+Nodoka4318@users.noreply.github.com>
2022-07-28 23:40:08 +0900

    アイテムプロパティの単一要素

---
+++ UI/ItemProperty/ItemPropertyWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mp4Utl.UI.ItemProperty {
+    /// <summary>
+    /// ItemPropertyWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ItemPropertyWindow : Window {
+        public ItemPropertyWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click_AddEffect(object sender, RoutedEventArgs e) {
+            var win = new AddEffect();
+            win.Show();
+        }
+    }
+}
